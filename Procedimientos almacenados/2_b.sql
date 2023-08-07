a56811a6011a09a301f0d89a16fd38279d37e260
Facundoheck <heckfacundo9@gmail.com>
2023-08-07 16:03:15 -0300

    PA 2b

---
+++ Procedimientos almacenados/2_b.sql
@@ -0,0 +1,51 @@
+CREATE PROCEDURE PA_ALTA_CURSOS(
+    @prof_id         INT,
+    @titulo          VARCHAR(70),
+    @nivel           VARCHAR(20),
+    @descripcion     VARCHAR(400)
+)
+AS
+	DECLARE @mensaje VARCHAR(MAX)
+
+
+-- inicio validaciones para el alta NO LAS HAGO PARA NO PERDER TIEMPO
+-- PONGO LA DE FK DE EJEMPLO
+IF NOT EXISTS (SELECT 1 FROM PROFESORES AS p WHERE p.prof_id = @prof_id)
+BEGIN
+	PRINT 'No existe el ID del profesor. Verifique los datos.'
+	RETURN -1
+END
+
+IF LEN(@titulo) > 70
+BEGIN
+	PRINT 'El título no puede superar los 70 caracteres. Verifique los datos.'
+	RETURN -1	
+END
+
+--COMIENZO CON EL ALTA
+
+BEGIN TRY
+INSERT INTO CURSOS
+(
+	prof_id,
+	titulo,
+	nivel,
+	descripcion
+)
+VALUES
+(
+	@prof_id,
+	@titulo,
+	@nivel,
+	@descripcion
+)
+	
+END TRY
+BEGIN CATCH
+	SELECT @mensaje = 'Ocurrió un error al intentar registar el curso: ' + ERROR_MESSAGE() 
+	PRINT @mensaje
+	RETURN -1
+END CATCH
+
+RETURN 0
+GO
