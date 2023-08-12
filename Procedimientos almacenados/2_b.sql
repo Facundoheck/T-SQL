bc015f1bb280b29f4740e7dfd6f78ec88f4c9db0
Facundo Heck <123607886+Facundoheck@users.noreply.github.com>
2023-08-12 15:13:47 -0300

    Update 2_b.sql

--- Procedimientos almacenados/2_b.sql
+++ Procedimientos almacenados/2_b.sql
@@ -7,9 +7,6 @@ CREATE PROCEDURE PA_ALTA_CURSOS(
 AS
 	DECLARE @mensaje VARCHAR(MAX)
 
-
--- inicio validaciones para el alta NO LAS HAGO PARA NO PERDER TIEMPO
--- PONGO LA DE FK DE EJEMPLO
 IF NOT EXISTS (SELECT 1 FROM PROFESORES AS p WHERE p.prof_id = @prof_id)
 BEGIN
 	PRINT 'No existe el ID del profesor. Verifique los datos.'
@@ -18,7 +15,7 @@ END
 
 IF LEN(@titulo) > 70
 BEGIN
-	PRINT 'El título no puede superar los 70 caracteres. Verifique los datos.'
+	PRINT 'El tÃ­tulo no puede superar los 70 caracteres. Verifique los datos.'
 	RETURN -1	
 END
 
@@ -42,7 +39,7 @@ VALUES
 	
 END TRY
 BEGIN CATCH
-	SELECT @mensaje = 'Ocurrió un error al intentar registar el curso: ' + ERROR_MESSAGE() 
+	SELECT @mensaje = 'OcurriÃ³ un error al intentar registar el curso: ' + ERROR_MESSAGE() 
 	PRINT @mensaje
 	RETURN -1
 END CATCH
