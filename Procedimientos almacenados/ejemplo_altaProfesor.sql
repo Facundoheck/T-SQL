1da57ae28f8cabd6edb8781c7aa5973574520817
Facundoheck <heckfacundo9@gmail.com>
2023-08-16 17:24:11 -0300

    ejemplo alta profesor

--- Procedimientos almacenados/ejemplo_altaProfesor.sql
+++ Procedimientos almacenados/ejemplo_altaProfesor.sql
@@ -6,3 +6,12 @@ EXEC altaProfesor
 	@fecha_nac = '20000202',
 	@direccion = 'Maipú 120',
 	@telefono = '341332211';
+
+EXEC altaProfesor 
+	@prof_id = '18',
+	@carrera_id = '3',
+	@nombre = 'Santiago',
+	@apellido = 'Arteche',
+	@fecha_nac = '19980203',
+	@direccion = 'Santa Fe 120',
+	@telefono = '341532211';
